2ca263a8547fcdddf56dea6a4e8eaf1012eb3ce2
eliasdabes <eliasab2@ac.sce.ac.il>
2024-12-16 00:47:57 +0200

    Home Page

---
+++ eBookStore/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using eBookStore.Models;
+using eBookStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["defaultConnectionString"].ConnectionString;
+        // GET: Home
+        public ActionResult HomePage()
+        {
+            BookViewModel bookViewModel = new BookViewModel
+            {
+                book = new Book(),
+                booksList = getBooksList()
+            };
+            return View(bookViewModel);
+        }
+
+        private List<Book> getBooksList()
+        {
+            List<Book> booksList = new List<Book>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM books";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Book book = new Book
+                            {
+                                id = Convert.ToInt32(reader["id"]),
+                                title = reader["title"].ToString(),
+                                //authors = new List<string>(),
+                                publisher = reader["publisher"].ToString(),
+                                priceForBorrowing = Convert.ToDecimal(reader["priceForBorrowing"]),
+                                priceForBuying = Convert.ToDecimal(reader["priceForBuying"]),
+                                priceSaleForBorrowing = reader["priceSaleForBorrowing"] != DBNull.Value ? Convert.ToDecimal(reader["priceSaleForBorrowing"]) : (decimal?)null,
+                                priceSaleForBuying = reader["priceSaleForBuying"] != DBNull.Value ? Convert.ToDecimal(reader["priceSaleForBuying"]) : (decimal?)null,
+                                yearOfPublishing = Convert.ToInt32(reader["yearOfPublishing"]),
+                                coverImagePath = reader["coverImagePath"].ToString(),
+                                ageLimitation = reader["ageLimitation"].ToString(),
+                                quantityInStock = Convert.ToInt32(reader["quantityInStock"]),
+                                popularity = Convert.ToInt32(reader["popularity"]),
+                                //dateSale = reader["dateSale"] != DBNull.Value ? Convert.ToDateTime(reader["dateSale"]) : (DateTime?)null                            
+                            };
+                            string sqlQuery2 = "SELECT * FROM authors WHERE bookId = @bookId";
+                            using (SqlCommand command2 = new SqlCommand(sqlQuery2, connection))
+                            {
+                                command2.Parameters.AddWithValue("@bookId", book.id);
+                                using (SqlDataReader reader2 = command2.ExecuteReader())
+                                {
+                                    while (reader2.Read())
+                                    {
+                                        Author author2 = new Author
+                                        {
+                                            authorName = reader2["authorName"].ToString(),
+                                            bookId = Convert.ToInt32(reader2["bookId"])
+                                        };
+                                        book.authors.Add(author2);
+                                    }
+                                }
+                            }
+
+                            booksList.Add(book);
+                        }
+                    }
+                }
+            }
+            return booksList;
+        }
+    }
+}
